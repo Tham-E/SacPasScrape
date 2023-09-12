ea8dd30e2f11003c7159cd45f8b862ff5e2242d5
Tham-E <enoch.tham@wildlife.ca.gov>
2023-09-12 10:32:35 -0700

    Removing Unnecessary files

--- VERNvsMSD.R
+++
@@ -1,9 +0,0 @@
-library(cder)
-
-VNS<-cdec_query("VNS",41,"D",as.Date("2001-01-01"),Sys.Date())
-MSD<-cdec_query("MSD",41,"D",as.Date("2001-01-01"),Sys.Date())
-SJR<-cdec_query("SJR",20,"E",as.Date("2001-01-01"),Sys.Date())%>%
-  mutate(Date=format(x = DateTime, format="%Y-%m-%d"))
-SJR <-  aggregate(Value ~ Date, data=SJR, FUN=mean)
-
-
